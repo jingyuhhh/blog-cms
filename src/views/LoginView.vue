aeee5a58da04a1abf81974dceda1fb9d308974da
火山火山 <2741547963@qq.com>
2023-07-16 11:09:40 +0800

    :white_check_mark: 完成登录页面

---
+++ src/views/LoginView.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+import axios from "axios";
+const username = ref("");
+const psw = ref("");
+async function handleLogin(){
+  const data = {
+    username,
+    psw
+  }
+  await axios.post("/api/user",data);
+}
+
+</script>
+
+<template>
+  <div class="main">
+    <div class="center">
+      <div class="content">
+        <div class="title">登录</div>
+        <el-input class="username" type="text" placeholder="请输入用户名" v-model="username"></el-input>
+        <el-input class="psw" type="password" placeholder="请输入密码" v-model="psw" show-password></el-input>
+        <el-button type="primary" @click="handleLogin" >提交</el-button>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<style lang="scss" >
+  .main {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    margin: 0;
+    padding: 0;
+    align-items: center;
+    justify-content: center;
+    background-image: url("../assets/bg.jpg");
+    opacity: 50%;
+    >.center{
+      background-color: white;
+      height: 355px;
+      width: 347px;
+      display: flex;
+      justify-content: center;
+      box-shadow: 0 0 16px 6px rgba(0,0,0,0.5);
+
+      >.content{
+        width: 70%;
+        display: grid;
+        place-items: center;
+        >.title{
+          font-size: 20px;
+          margin-top: 30px;
+        }
+
+      }
+      
+    }
+  }
+</style>
